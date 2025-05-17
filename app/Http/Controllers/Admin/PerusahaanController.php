71839a4c47cab973d5824ed2fa2df686f5c37606
Oktavian Eka Ramadhan <72781555+Oktavian19@users.noreply.github.com>
2025-05-17 09:13:55 +0700

    Revert "fix: tambah Perusahaan Mitra create_ajax didn't pass programStudi"
    This reverts commit 38c2a797d084de5719d0cf6e7961a4ba2480f370.

--- app/Http/Controllers/Admin/PerusahaanController.php
+++ app/Http/Controllers/Admin/PerusahaanController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\PerusahaanMitra;
-use App\Models\ProgramStudi;
 use Illuminate\Http\Request;
 use Yajra\DataTables\Facades\DataTables;
 use Illuminate\Support\Facades\Validator;
@@ -57,8 +56,7 @@ class PerusahaanController extends Controller
 
     public function create_ajax()
     {
-        $programStudi = ProgramStudi::all();
-        return view('admin.perusahaan.create_ajax', compact('programStudi'));
+        return view('admin.perusahaan.create_ajax');
     }
 
     public function store_ajax(Request $request)
