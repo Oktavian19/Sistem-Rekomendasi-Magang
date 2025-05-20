a362806a7041965b7cbefaacadcd62ceb1776b9e
Oktavian Eka Ramadhan <72781555+Oktavian19@users.noreply.github.com>
2025-05-20 12:13:36 +0700

    fix-lowongan-pagination

--- resources/views/mahasiswa/magang/lowongan.blade.php
+++ resources/views/mahasiswa/magang/lowongan.blade.php
@@ -163,20 +163,7 @@
                     <!-- Pagination -->
                     <nav aria-label="Page navigation">
                         <ul class="pagination justify-content-center mt-4">
-                            <li class="page-item disabled">
-                                <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
-                            </li>
-                            <li class="page-item active"><a class="page-link" href="#">1</a></li>
-                            <li class="page-item"><a class="page-link" href="#">2</a></li>
-                            <li class="page-item"><a class="page-link" href="#">3</a></li>
-                            <li class="page-item disabled">
-                                <a class="page-link" href="#">...</a>
-                            </li>
-                            <li class="page-item"><a class="page-link" href="#">59</a></li>
-                            <li class="page-item"><a class="page-link" href="#">60</a></li>
-                            <li class="page-item">
-                                <a class="page-link" href="#">Next</a>
-                            </li>
+                            {{ $lowongans->links() }}
                         </ul>
                     </nav>
                 </div>
