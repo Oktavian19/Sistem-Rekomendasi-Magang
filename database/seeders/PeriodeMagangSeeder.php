8cb485d4757e064351a189aec1d8cc053208d760
Hamdan Azizul Hakim <hamdanizul24@gmail.com>
2025-05-24 23:31:58 +0700

    add: column status on PeriodeMagang table

--- database/seeders/PeriodeMagangSeeder.php
+++ database/seeders/PeriodeMagangSeeder.php
@@ -11,10 +11,10 @@ class PeriodeMagangSeeder extends Seeder
     public function run(): void
     {
         DB::table('periode_magang')->insert([
-            ['nama_periode' => 'Magang Semester Ganjil 2023/2024', 'tanggal_mulai' => '2023-08-10','tanggal_selesai' => '2023-12-25','created_at' => now()],
-            ['nama_periode' => 'Magang Semester Genap 2023/2024', 'tanggal_mulai' => '2024-01-10','tanggal_selesai' => '2024-07-25','created_at' => now()],
-            ['nama_periode' => 'Magang Semester Ganjil 2024/2025', 'tanggal_mulai' => '2024-08-10','tanggal_selesai' => '2024-12-25','created_at' => now()],
-            ['nama_periode' => 'Magang Semester Genap 2024/2025', 'tanggal_mulai' => '2025-01-10','tanggal_selesai' => '2025-07-25','created_at' => now()],
+            ['nama_periode' => 'Magang Semester Ganjil 2023/2024', 'tanggal_mulai' => '2023-08-10','tanggal_selesai' => '2023-12-25','created_at' => now(), 'status' => 'non-aktif'],
+            ['nama_periode' => 'Magang Semester Genap 2023/2024', 'tanggal_mulai' => '2024-01-10','tanggal_selesai' => '2024-07-25','created_at' => now(), 'status' => 'non-aktif'],
+            ['nama_periode' => 'Magang Semester Ganjil 2024/2025', 'tanggal_mulai' => '2024-08-10','tanggal_selesai' => '2024-12-25','created_at' => now(), 'status' => 'non-aktif'],
+            ['nama_periode' => 'Magang Semester Genap 2024/2025', 'tanggal_mulai' => '2025-01-10','tanggal_selesai' => '2025-07-25','created_at' => now(), 'status' => 'aktif'],
         ]);
     }
 }
