13db00cbe2da2b5a4f2006e948aaa28b5d519329
Oktavian Eka Ramadhan <72781555+Oktavian19@users.noreply.github.com>
2025-06-03 22:34:05 +0700

    fix:minlength telepon perusahaan

--- resources/views/admin/perusahaan/edit_ajax.blade.php
+++ resources/views/admin/perusahaan/edit_ajax.blade.php
@@ -165,6 +165,7 @@
                     },
                     telepon: {
                         required: true,
+                        minlength: 10,
                         maxlength: 20,
                         digits: true
                     },
@@ -200,6 +201,7 @@
                     },
                     telepon: {
                         required: "Telepon wajib diisi.",
+                        minlength: "Minimal 10 digit.",
                         maxlength: "Maksimal 20 digit.",
                         digits: "Hanya angka yang diperbolehkan."
                     },
