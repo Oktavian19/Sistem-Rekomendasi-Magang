bb8e9111c0f21cb7661f9e688b5e4b704e7b79ca
Oktavian Eka Ramadhan <72781555+Oktavian19@users.noreply.github.com>
2025-06-03 20:31:54 +0700

    fix:filtering lamaran belum plot dosen dan detail magang pada dosen pembimbing

--- resources/views/admin/lamaran/index.blade.php
+++ resources/views/admin/lamaran/index.blade.php
@@ -103,7 +103,7 @@
                                         @if ($item->magang && $item->magang->dosenPembimbing)
                                             {{ $item->magang->dosenPembimbing->nama }}
                                         @else
-                                            <span class="text-muted">Belum ada dosen</span>
+                                            <span class="text-muted">(Belum ada dosen)</span>
                                         @endif
                                     @else
                                         <span class="text-muted">-</span>
