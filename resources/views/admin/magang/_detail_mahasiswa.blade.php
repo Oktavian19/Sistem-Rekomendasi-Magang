606eab7170427bb55d0d7b1c6aa4924d8b796ca6
Hamdan Azizul Hakim <hamdanizul24@gmail.com>
2025-06-11 22:48:16 +0700

    fix: detail mahasiswa on pelaksanaan magang

--- resources/views/admin/magang/_detail_mahasiswa.blade.php
+++ resources/views/admin/magang/_detail_mahasiswa.blade.php
@@ -1,45 +1,100 @@
 <div class="row">
-    <div class="col-md-4">
-        <div class="text-center mb-3">
-            @if($magang->lamaran->mahasiswa->foto_profil)
-                <img src="{{ asset('storage/' . $magang->lamaran->mahasiswa->foto_profil) }}" 
-                     class="rounded-circle img-thumbnail" width="150" height="150" alt="Foto Profil">
-            @else
-                <div class="avatar avatar-xl rounded-circle bg-label-secondary">
-                    <span class="avatar-initials">
-                        {{ substr($magang->lamaran->mahasiswa->nama, 0, 1) }}
-                    </span>
-                </div>
-            @endif
+    <div class="col-md-12">
+        <h5>Informasi Mahasiswa</h5>
+        <div class="mb-3">
+            <table class="table table-sm table-borderless">
+                <tr>
+                    <th width="35%">Nama</th>
+                    <td>{{ $magang->lamaran->mahasiswa->nama }}</td>
+                </tr>
+                <tr>
+                    <th>NIM</th>
+                    <td>{{ $magang->lamaran->mahasiswa->nim }}</td>
+                </tr>
+                <tr>
+                    <th>Program Studi</th>
+                    <td>{{ $magang->lamaran->mahasiswa->programStudi->nama_program_studi ?? '-' }}</td>
+                </tr>
+                <tr>
+                    <th>Email</th>
+                    <td>{{ $magang->lamaran->mahasiswa->email ?? '-' }}</td>
+                </tr>
+                <tr>
+                    <th>No. HP</th>
+                    <td>{{ $magang->lamaran->mahasiswa->no_hp ?? '-' }}</td>
+                </tr>
+            </table>
         </div>
-    </div>
-    <div class="col-md-8">
-        <h4>{{ $magang->lamaran->mahasiswa->nama }}</h4>
-        <table class="table table-sm">
-            <tr>
-                <th width="30%">NIM</th>
-                <td>{{ $magang->lamaran->mahasiswa->nim }}</td>
-            </tr>
-            <tr>
-                <th>Program Studi</th>
-                <td>{{ $magang->lamaran->mahasiswa->programStudi->nama_program_studi ?? '-' }}</td>
-            </tr>
-            <tr>
-                <th>Email</th>
-                <td>{{ $magang->lamaran->mahasiswa->user->email ?? '-' }}</td>
-            </tr>
-            <tr>
-                <th>No. HP</th>
-                <td>{{ $magang->lamaran->mahasiswa->no_hp ?? '-' }}</td>
-            </tr>
-            <tr>
-                <th>Alamat</th>
-                <td>{{ $magang->lamaran->mahasiswa->alamat ?? '-' }}</td>
-            </tr>
-            <tr>
-                <th>Jenis Kelamin</th>
-                <td>{{ $magang->lamaran->mahasiswa->jenis_kelamin == 'L' ? 'Laki-laki' : 'Perempuan' }}</td>
-            </tr>
-        </table>
+
+        <!-- Pengalaman Kerja Section -->
+        <h5>Pengalaman Kerja</h5>
+        @if ($magang->lamaran->mahasiswa->pengalamanKerja->count() > 0)
+            <div class="table-responsive">
+                <table class="table table-bordered table-hover">
+                    <thead>
+                        <tr>
+                            <th>Nama Perusahaan</th>
+                            <th>Posisi</th>
+                            <th>Tanggal Mulai</th>
+                            <th>Tanggal Selesai</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($magang->lamaran->mahasiswa->pengalamanKerja as $pengalaman)
+                            <tr>
+                                <td>{{ $pengalaman->perusahaan }}</td>
+                                <td>{{ $pengalaman->nama_posisi }}</td>
+                                <td>{{ \Carbon\Carbon::parse($pengalaman->tanggal_mulai)->translatedFormat('d F Y') }}
+                                </td>
+                                <td>{{ \Carbon\Carbon::parse($pengalaman->tanggal_selesai)->translatedFormat('d F Y') }}
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        @else
+            <div class="alert alert-info">Mahasiswa belum memiliki pengalaman kerja</div>
+        @endif
+
+        <!-- Dokumen Section -->
+        <h5 class="mt-4">Dokumen Pendukung</h5>
+        @if ($magang->lamaran->mahasiswa->dokumen->count() > 0)
+            <div class="table-responsive">
+                <table class="table table-bordered table-hover">
+                    <thead>
+                        <tr>
+                            <th>Jenis Dokumen</th>
+                            <th>File</th>
+                            <th>Tanggal Upload</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($magang->lamaran->mahasiswa->dokumen as $dokumen)
+                            <tr>
+                                <td>{{ $dokumen->jenis_dokumen }}</td>
+                                <td>
+                                    @php
+                                        $filename = basename($dokumen->path_file);
+                                    @endphp
+                                    {{ $filename }}
+                                </td>
+                                <td>{{ \Carbon\Carbon::parse($dokumen->tanggal_upload)->translatedFormat('d F Y') }}
+                                </td>
+                                <td>
+                                    <a href="{{ asset('storage/' . $dokumen->path_file) }}" target="_blank"
+                                        class="btn btn-sm btn-info" title="Preview">
+                                        <i class="fas fa-eye"></i>
+                                    </a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        @else
+            <div class="alert alert-info">Mahasiswa belum mengunggah dokumen pendukung</div>
+        @endif
     </div>
 </div>
