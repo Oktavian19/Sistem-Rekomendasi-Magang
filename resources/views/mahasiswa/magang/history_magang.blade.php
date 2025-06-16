f6ea905de65c2bb492cd1aeaafc7141f60be6a20
BillyMaulanaFerdinan <billy.maulana2020@gmail.com>
2025-06-17 02:01:35 +0700

    add notifikasi feedback riwayat mahasiswa

--- resources/views/mahasiswa/magang/history_magang.blade.php
+++ resources/views/mahasiswa/magang/history_magang.blade.php
@@ -203,4 +203,15 @@
             });
         });
     </script>
+    @if (session('success'))
+        <script>
+            Swal.fire({
+                icon: 'success',
+                title: 'Berhasil!',
+                text: '{{ session('success') }}',
+                confirmButtonColor: '#3085d6',
+                confirmButtonText: 'OK'
+            });
+        </script>
+    @endif
 @endpush
